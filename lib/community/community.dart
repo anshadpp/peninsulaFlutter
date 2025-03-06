a5dab3d234dad5c5377bdfabd9b6a16167e50275
ANSHAD  P P <71533560+anshadpp@users.noreply.github.com>
2025-03-06 16:49:42 +0530

    added login,registration,some part of community

--- lib/community/community.dart
+++ lib/community/community.dart
@@ -24,79 +24,38 @@ class _CommunityState extends State<Community> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Container(
-      color: Colors.white,             
-      padding: const EdgeInsets.all(8), 
-      child: Row(
-        children: [
-          CircleAvatar(
-            radius: 20,
-            backgroundImage: AssetImage(
-              'assets/images/facebookLogo.png', 
+              color: Colors.white,
+              padding: const EdgeInsets.all(8),
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    radius: 20,
+                    backgroundImage: AssetImage(
+                      'assets/images/facebookLogo.png',
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  Text(
+                    'Facebook',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black,
+                    ),
+                  ),
+                  const Spacer(),
+                  Icon(
+                    Icons.more_vert,
+                    color: Colors.black,
+                  ),
+                ],
+              ),
             ),
-          ),
-          const SizedBox(width: 8),
-          Text(
-            'Facebook',
-            style: TextStyle(
-              fontSize: 16,
-              color: Colors.black,
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: Image.asset(
+                'assets/images/aa.png',
+              ),
             ),
-          ),
-          const Spacer(),
-          Icon(
-            Icons.more_vert,
-            color: Colors.black,
-          ),
-        ],
-      ),
-    ),
-    SizedBox(
-  width: MediaQuery.of(context).size.width,
-  child: Image.asset(
-              'assets/images/aa.png', 
-            ),
-),
-
-
-
-            // Container(
-            //   width: widthRatio * 393,
-            //   height: heightRatio * 48,
-            //   decoration: BoxDecoration(
-            //     border: Border(
-            //       top: BorderSide(
-            //         color: const Color.fromARGB(255, 141, 64, 64),
-            //       ),
-            //     ),
-            //     color: Colors.white,
-            //   ),
-            //   child: Stack(
-            //     children: [
-            //       Positioned(
-            //         left: widthRatio * 11,
-            //         child: CircleAvatar(
-            //           backgroundColor: Colors.blue,
-            //           radius: widthRatio * 20,
-            //         ),
-            //       ),
-            //       Positioned(
-            //         // top: heightRatio * 224,
-            //         left: widthRatio * 54,
-            //           child: Container(
-            //             // alignment: Alignment.center,
-            //             height: heightRatio * 48,
-            //             width: widthRatio * 393,
-            //             child: Text(
-            //                                 "data",
-            //                                 style: TextStyle(color: Colors.black26),
-            //                               ),
-            //           )),
-            //       Positioned(
-            //         left: widthRatio * 376,
-            //         child: Icon(Icons.more_vert_rounded)),
-            //     ],
-            //   ),
-            // ),
           ],
         ),
       ),
